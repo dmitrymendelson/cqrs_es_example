588d31e4a8f53c4148698c764eb5457350764109
Dmitrii Sporysh <dmitrysporysh@gmail.com>
2020-06-21 18:12:30 +0300

    add check-in command and handler

--- src/Domain/Aggregate/Building.php
+++ src/Domain/Aggregate/Building.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Building\Domain\Aggregate;
 
 use Building\Domain\DomainEvent\NewBuildingWasRegistered;
+use Building\Domain\DomainEvent\UserCheckedIn;
 use Prooph\EventSourcing\AggregateRoot;
 use Rhumsaa\Uuid\Uuid;
 
@@ -24,11 +25,9 @@ final class Building extends AggregateRoot
     {
         $self = new self();
 
-        $self->recordThat(NewBuildingWasRegistered::occur(
-            (string) Uuid::uuid4(),
-            [
-                'name' => $name
-            ]
+        $self->recordThat(NewBuildingWasRegistered::fromBuildingIdAndName(
+            Uuid::uuid4(),
+            $name
         ));
 
         return $self;
@@ -36,7 +35,10 @@ final class Building extends AggregateRoot
 
     public function checkInUser(string $username)
     {
-        // @TODO to be implemented
+        $this->recordThat(UserCheckedIn::fromBuildingIdAndUsername(
+            $this->uuid,
+            $username
+        ));
     }
 
     public function checkOutUser(string $username)
@@ -46,8 +48,13 @@ final class Building extends AggregateRoot
 
     public function whenNewBuildingWasRegistered(NewBuildingWasRegistered $event)
     {
-        $this->uuid = Uuid::fromString($event->aggregateId());
+        $this->uuid = $event->uuid();
         $this->name = $event->name();
+    }
+
+    public function whenUserCheckedIn(UserCheckedIn $event)
+    {
+        // nothing
     }
 
     /**
